c4e9965ac55194411f0080930a4ab1f24ec1bc0d
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-05-22 20:11:59 +0530

    parity

--- python_code/primitive_types/parity.py
+++ python_code/primitive_types/parity.py
@@ -1,6 +1,17 @@
-def parity(x: int) -> int:
-    print(x)
-    pass
+def parity_1(x: int) -> int:
+    result = 0
+    while x:
+        result ^= x&1
+        x >>= 1
+    return result
+
+def parity_2(x : int) -> int:
+    result = 0
+    while x:
+        result ^= 1
+        x &= x-1
+    return result
 
 if __name__ == '__main__':
-    parity(5)
+    print(parity_1(31))
+    print(parity_1(31))
