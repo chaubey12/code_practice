9f3a506ce7e7e73e5c820cd35d9a34dabc4b5bf2
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-05-25 07:27:27 +0530

    python codes

--- python_code/primitive_types/parity.py
+++ python_code/primitive_types/parity.py
@@ -12,6 +12,19 @@ def parity_2(x : int) -> int:
         x &= x-1
     return result
 
+PRECOMPUTED_PARITY = []
+def populate_cache():
+    for i in range(2**16):
+        PRECOMPUTED_PARITY.append(parity_2(i))
+
+def parity_with_cache(x: int) -> int:
+    mask_size = 16
+    bit_mask = 0xFFFF
+    return (PRECOMPUTED_PARITY[x >> (3*mask_size)]^
+        PRECOMPUTED_PARITY[(x >> (2*mask_size)) & bit_mask]^
+        PRECOMPUTED_PARITY[(x >> mask_size) & bit_mask]^
+        PRECOMPUTED_PARITY[x & bit_mask])
+
 if __name__ == '__main__':
-    print(parity_1(31))
-    print(parity_1(31))
+    populate_cache()
+    print(parity_with_cache(2**63))
