e8ac58f38ad445203e45782c2869b27585d76d10
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-06-21 23:04:10 +0530

    k way merge

--- cpp_code/k_way_merge/merge_k_lists.cpp
+++ cpp_code/k_way_merge/merge_k_lists.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 class ListNode{
-public:
-    int value;
+public: 
+    int value=0;
     ListNode* next;
     ListNode(int value){
         this->value = value;
@@ -14,24 +14,22 @@ public:
     }
 };
 
-class MergeKSortedLists{
+class MergeKLists{
 public:
     struct valueCompare{
-        bool operator()(const ListNode* x, const ListNode* y){
-            return x->value > y->value;
+        bool operator()(const ListNode* a, const ListNode* b){
+            return a->value > b->value;
         }
     };
 
-    static ListNode* merge(const vector<ListNode*>& lists){
+    static ListNode* merge(vector<ListNode*> lists){
         priority_queue<ListNode*, vector<ListNode*>, valueCompare> minHeap;
-        for(auto list : lists){
-            if(list != nullptr){
-                minHeap.push(list);
-            }
+        for(auto root : lists){
+            minHeap.push(root);
         }
         ListNode* resultHead = nullptr, *resultTail = nullptr;
         while(!minHeap.empty()){
-            ListNode* node = minHeap.top();
+            auto node = minHeap.top();
             minHeap.pop();
             if(resultHead == nullptr){
                 resultHead = resultTail = node;
@@ -44,10 +42,26 @@ public:
             }
         }
         return resultHead;
-    }
+    } 
 };
 
-
 int main(){
+    ListNode* l1 = new ListNode(1);
+    l1->next = new ListNode(5);
+    l1->next->next = new ListNode(9);
+    ListNode* l2 = new ListNode(2);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(10);
+    ListNode* l3 = new ListNode(3);
+    l2->next = new ListNode(7);
+    l2->next->next = new ListNode(11);
+    vector<ListNode*> l_arr = {l1, l2, l3};
+    auto list = MergeKLists::merge(l_arr);
+    while(list != nullptr){
+        cout << list->value << " ";
+        list = list->next;
+    }
+    cout << endl;
+
     return 0;
 }
