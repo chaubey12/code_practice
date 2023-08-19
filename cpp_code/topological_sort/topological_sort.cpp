a9d09ec7a3c85596e3f7f031a89a57ecfdbbc5f3
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-08-19 22:07:04 +0530

    topologival sort

--- cpp_code/topological_sort/topological_sort.cpp
+++ cpp_code/topological_sort/topological_sort.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <unordered_map>
-#include <string>
+#include <queue>
 
 using namespace std;
 
+
 class TopologicalSort{
 public:
-    static vector<int> sort(int vertices, vector<vector<int>>& edges){
+    static vector<int> order(const vector<vector<int>>& edges, int vertices){
         vector<int> sortedOrder;
-        unordered_map<int, int> inDegree;
         unordered_map<int, vector<int>> graph;
+        unordered_map<int, int> inDegree;
         for(int i = 0; i < vertices; i++){
             inDegree[i] = 0;
             graph[i] = vector<int>();
         }
-        for(int i = 0; i < edges.size(); i++){
-            int parent = edges[i][0], child = edges[i][1];
-            inDegree[child]++;
+        for(auto edge : edges){
+            int parent = edge[0];
+            int child = edge[1];
             graph[parent].push_back(child);
+            inDegree[child]++;
         }
         queue<int> sources;
-        for(auto entry : inDegree){
-            if(entry.second == 0){
-                sources.push(entry.first);
+        for(auto edge : inDegree){
+            if(edge.second == 0){
+                sources.push(edge.first);
             }
         }
         while(!sources.empty()){
-            int vertex = sources.front();
+            int parent = sources.front();
             sources.pop();
-            sortedOrder.push_back(vertex);
-            vector<int> children = graph[vertex];
+            sortedOrder.push_back(parent);
+            vector<int> children = graph[parent];
             for(auto child : children){
                 inDegree[child]--;
                 if(inDegree[child] == 0){
@@ -39,6 +40,7 @@ public:
                 }
             }
         }
+
         if(sortedOrder.size() != vertices){
             return vector<int>();
         }
@@ -46,7 +48,28 @@ public:
     }
 };
 
-int main(){
 
+int main(){
+    vector<vector<int>> edges1 = {{3,2}, {3,0}, {2,0}, {2,1}};
+    int vertices1 = 4;
+    auto result = TopologicalSort::order(edges1, vertices1);
+    for(int i : result){
+        cout << i << " ";
+    }
+    cout << endl;
+    vector<vector<int>> edges2 = {{4, 2}, {4, 3}, {2, 0}, {2, 1}, {3, 1}};
+    int vertices2 = 5;
+    result = TopologicalSort::order(edges2, vertices2);
+    for(int i : result){
+        cout << i << " ";
+    }
+    cout << endl;
+    vector<vector<int>> edges3 = {{6, 4}, {6, 2}, {5, 3}, {5, 4}, {3, 0}, {3, 1}, {3, 2}, {4, 1}};
+    int vertices3 = 7;
+    result = TopologicalSort::order(edges3, vertices3);
+    for(int i : result){
+        cout << i << " ";
+    }
+    cout << endl;
     return 0;
 }
