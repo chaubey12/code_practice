1b83ea96679ef8c0966da6ccfda07dc275d23e8a
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-08-23 07:49:50 +0530

    1 replacement

---
+++ cpp_code/01_sliding_window/subarray_one_replace.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class SubarrayOneReplacement{
+public:
+    static int find(vector<int>& arr, int k){
+        int windowStart=0;
+        int maxOneCount=0, maxLen=0;
+        for(int windowEnd=0; windowEnd < arr.size(); windowEnd++){
+            if(arr[windowEnd] == 1){
+                maxOneCount++;
+            }
+            if(windowEnd - windowStart + 1 -maxOneCount > k){
+                if(arr[windowStart] == 1){
+                    maxOneCount--;
+                }
+                windowStart++;
+            }
+            maxLen = max(maxLen, windowEnd - windowStart + 1);
+        }
+        return maxLen;
+    }
+};
+
+int main(){
+    vector<int> arr = {0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1};
+    int k = 3;
+    cout << SubarrayOneReplacement::find(arr, k) << endl;
+    return 0;
+}
