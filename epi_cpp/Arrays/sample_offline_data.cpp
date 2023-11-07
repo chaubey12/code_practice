8b8bc36552545492ba3a73fa58590592b230df63
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-11-08 00:51:53 +0530

    correction

--- epi_cpp/Arrays/sample_offline_data.cpp
+++ epi_cpp/Arrays/sample_offline_data.cpp
@@ -8,7 +8,7 @@ void RandomSampling(vector<int>* A_ptr, int k){
     vector<int>& A = *A_ptr;
     default_random_engine seed((random_device())());
     for(int i=0; i < k; ++i){
-        swap(A[i], A[uniform_int_distribution<int>(i, A.size())(seed)]);
+        swap(A[i], A[uniform_int_distribution<int>(i, static_cast<int>(A.size() - 1))(seed)]);
     }
 }
 
