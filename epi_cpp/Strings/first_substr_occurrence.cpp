6275866813cd4a7fdf58607d9d9e2e82b0a439be
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-11-13 08:34:30 +0530

    rabin karp correction

--- epi_cpp/Strings/first_substr_occurrence.cpp
+++ epi_cpp/Strings/first_substr_occurrence.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 
-
 using  namespace std;
 
 int RabinKarp(const string& t, const string& s){
-    if(s.length() > t.length()){
+    if(s.size() > t.size()){
         return -1;
     }
-    const int kBase = 26;
+    const int kBase = 0;
     int t_hash=0, s_hash=0, power_s=1;
-    for(int i=0; i<s.size(); ++i){
-        power_s = i ? power_s*kBase : 1;
-        t_hash = t_hash*kBase + t[i];
+    for(int i=0; i < s.size(); ++i){
+        power_s = i?power_s*kBase:i;
         s_hash = s_hash*kBase + s[i];
+        t_hash = t_hash*kBase + t[i];
     }
-    for(int i = s.size(); i < t.size(); ++i){
-        if(t_hash == s_hash && !t.compare(i-s.size(), s.size(), s)){
+    for(int i=s.size(); i < t.size(); ++i){
+        if((s_hash == t_hash) && (!t.compare(i-s.size(), s.size(), s))){
             return i-s.size();
         }
         t_hash -= t[i-s.size()]*power_s;
-        t_hash += t_hash*kBase + t[i];
+        t_hash = t_hash*kBase + t[i];
     }
-    if((t_hash == s_hash) && (t.compare(t.size() - s.size(), s.size(), s) == 0)){
+    if((s_hash == t_hash) && (t.compare(t.size() - s.size(), s.size(), s) == 0)){
         return t.size() - s.size();
     }
     return -1;
