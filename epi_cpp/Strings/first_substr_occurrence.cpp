8ba702283ec8fcb0ce7115018f02445d7e562f3d
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-11-13 08:18:11 +0530

    rabin karp

---
+++ epi_cpp/Strings/first_substr_occurrence.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+using  namespace std;
+
+int RabinKarp(const string& t, const string& s){
+    if(s.length() > t.length()){
+        return -1;
+    }
+    const int kBase = 26;
+    int t_hash=0, s_hash=0, power_s=1;
+    for(int i=0; i<s.size(); ++i){
+        power_s = i ? power_s*kBase : 1;
+        t_hash = t_hash*kBase + t[i];
+        s_hash = s_hash*kBase + s[i];
+    }
+    for(int i = s.size(); i < t.size(); ++i){
+        if(t_hash == s_hash && !t.compare(i-s.size(), s.size(), s)){
+            return i-s.size();
+        }
+        t_hash -= t[i-s.size()]*power_s;
+        t_hash += t_hash*kBase + t[i];
+    }
+    if((t_hash == s_hash) && (t.compare(t.size() - s.size(), s.size(), s) == 0)){
+        return t.size() - s.size();
+    }
+    return -1;
+}
+
+int main(){
+    string s = "stay";
+    string t = "thisisthemostsignificantpartofthelifewhichstayswiththeindividual";
+    cout << "The given string is at position: " << RabinKarp(t, s) << endl;
+    return 0;
+}
