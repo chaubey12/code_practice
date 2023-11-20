c2871366d898e93f02ce14be181d77f562e61f25
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-11-20 09:11:34 +0530

    first missing positive

---
+++ epi_cpp/HonorsClass/first_missing_positive.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int FirstMissingPositive(vector<int>& A){
+    size_t i = 0;
+    while(i < A.size()){
+        if((A[i] > 0) &&  (A[i] < A.size()) && (A[i] != A[A[i] - 1])){
+            swap(A[i], A[A[i] - 1]);
+        }else{
+            ++i;
+        }
+    }
+    for(i=0; i<A.size(); ++i){
+        if(A[i] != (i+1)){
+            return i+1;
+        }
+    }
+    return A.size()+1;
+}
+
+int main(){
+    vector<int> A = {3, 5, 4, -1, 5, 1, -1};
+    cout << FirstMissingPositive(A) << endl;
+    return 0;
+}
