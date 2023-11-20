508139b8e6bcab903382b6a6690c48a214a085b9
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-11-20 19:46:41 +0530

    multiply

---
+++ epi_cpp/Arrays/multiply.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> Multiply(vector<int> num1, vector<int> num2){
+    int sign = ((num1.front() < 0) ^ (num2.front())) < 0 ? -1 : 1;
+    num1.front() = abs(num1.front()), num2.front() = abs(num2.front());
+    vector<int> result(num1.size() + num2.size(), 0);
+    for(int i=num1.size()-1; i >= 0; --i){
+        for(int j=num2.size()-1; j >= 0; --j){
+            result[i+j+1] += num1[i]*num2[j];
+            result[i+j] += result[i+j+1]/10;
+            result[i+j+1] %= 10;
+        }
+    }
+    result = vector<int>{find_if_not(begin(result), end(result), [](int a){ return !a; }), end(result)};
+    if(result.empty()){
+        return {0};
+    }
+    result.front() *= sign;
+    return result;
+}
+
+int main(){
+    vector<int> num1 = {1,5};
+    vector<int> num2 = {1,5};
+    auto result = Multiply(num1, num2);
+    for(auto i : result){
+        cout << i << " ";
+    } 
+    cout << endl;
+    return 0;
+}
