903ef63d238fbd6b519ad2334c4d7a20e7bcad62
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2023-12-25 15:31:13 +0530

    bfs matrix painting

---
+++ epi_cpp/Graphs/paint_matrix_bfs.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <memory>
+#include <array>
+#include <queue>
+
+using namespace std;
+
+void FlipColor(int x, int y, vector<deque<bool>>* A_ptr){
+    vector<deque<bool>>& A = *A_ptr;
+    const array<array<int, 2>, 4> kDirs = {{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}};
+    const bool color = A[x][y];
+    struct Coordinate{
+        int x, y;
+    };
+    queue<Coordinate> q;
+    A[x][y] = !color;
+    q.emplace(Coordinate{x, y});
+    while(!q.empty()){
+        Coordinate curr = q.front();
+        for(const array<int, 2>& s : kDirs){
+            const int next_x = curr.x + s[0], next_y = curr.y + s[1];
+            if(next_x >= 0 && next_x < A.size() && next_y >= 0 && next_y < A[next_x].size() && A[next_x][next_y] == color){
+                A[next_x][next_y] = !color;
+                q.emplace(Coordinate{next_x, next_y});
+            }
+        }
+        q.pop();
+    }
+}
+
+int main(){
+    vector<deque<bool>> A = {{true, false, true, false, false, false, true, true, true, true},
+                             {false, false, true, false, false, true, false, false, true, true},
+                             {true, true, true, false, false, true, true, false, true, true},
+                             {false, true, false, true, true, true, true, false, true, false},
+                             {true, false, true, false, false, false, false, true, false, false},
+                             {true, false, true, false, false, true, false, true, true, true},
+                             {false, false, false, false, true, false, true, false, false, true},
+                             {true, false, true, false, true, false, true, false, false, false},
+                             {true, false, true, true, false, false, false, true, true, true},
+                             {false, false, false, false, false, false, false, true, true, false}};
+    cout << "BEFORE FLIPPING" <<endl;
+    for(auto row : A){
+        for(auto elem : row){
+            cout << elem << " ";
+        }
+        cout << endl;
+    }
+    FlipColor(5, 4, &A);
+    cout << "AFTER FLIPPING" <<endl;
+    for(auto row : A){
+        for(auto elem : row){
+            cout << elem << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+}
