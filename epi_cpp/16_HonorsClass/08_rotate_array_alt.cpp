a4b74f7090e4301c74470f879908d6f3c64e17f5
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2024-02-05 08:25:46 +0530

    rotate array

--- epi_cpp/16_HonorsClass/08_rotate_array_alt.cpp
+++ epi_cpp/16_HonorsClass/08_rotate_array_alt.cpp
@@ -2,3 +2,25 @@
 #include <vector>
 
 using namespace std;
+
+void RotateArray(int i, vector<int>* A){
+    i %= A->size();
+    reverse(A->begin(), A->end());
+    reverse(A->begin(), A->begin() + i);
+    reverse(A->begin() + i, A->end());
+}
+
+int main(){
+    vector<int> A{4,5,1,2,3,4};
+    int rotate_amount = 2;
+    for(auto e : A){
+        cout << e << " ";
+    }
+    cout << endl;
+    RotateArray(rotate_amount, &A);
+    for(auto e : A){
+        cout << e << " ";
+    }
+    cout << endl;
+    return 0;
+}
