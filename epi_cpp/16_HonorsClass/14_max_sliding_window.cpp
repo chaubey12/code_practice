bef3f0b936abcc0dace8ce753660ccb10e1195b7
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2024-02-07 08:27:03 +0530

    max sliding window

--- epi_cpp/16_HonorsClass/14_max_sliding_window.cpp
+++ epi_cpp/16_HonorsClass/14_max_sliding_window.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -15,16 +16,16 @@ public:
             if(candidates_for_max_.back() >= x){
                 break;
             }
-            candidates_for_max_.pop_back();
+            candidates_for_max_.pop();
         }
-        candidates_for_max_.emplace_back(x);
+        candidates_for_max_.emplace(x);
     }
 
     T Dequeue(){
         if(!entries_.empty()){
             T result = entries_.front();
             if(result == candidates_for_max_.front()){
-                candidates_for_max_.pop_front();
+                candidates_for_max_.pop();
             }
             entries_.pop();
             return result;
@@ -58,8 +59,23 @@ struct TrafficElement{
 
 vector<TrafficElement> CalculateTrafficVolume(const vector<TrafficElement>& A, int w){
     QueueWithMax<TrafficElement> sliding_window;
+    vector<TrafficElement> maximum_volume;
+    for(const auto traffic_info : A){
+        sliding_window.Enqueue(traffic_info);
+        while(traffic_info.time - sliding_window.Max().time > w){
+            sliding_window.Dequeue();
+        }
+        maximum_volume.emplace_back(TrafficElement{traffic_info.time, sliding_window.Max().volume});
+    }
+    return maximum_volume;
 }
 
 int main(){
+    vector<TrafficElement> A{{0, 1.3}, {2, 2.5}, {3, 3.7}, {5, 1.4}, {6, 2.6}, {8, 2.2}, {9, 1.7}, {14, 1.1}};
+    int w = 5;
+    auto result = CalculateTrafficVolume(A, w);
+    for(auto elem : result){
+        cout << elem.time<<" ,"<<elem.volume<<endl;
+    }
     return 0;
 }
