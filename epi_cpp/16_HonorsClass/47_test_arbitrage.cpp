c0be93e194f57690d5b73be1dfcbc5dedcc1b954
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2024-03-03 19:49:24 +0530

    updated working

--- epi_cpp/16_HonorsClass/47_test_arbitrage.cpp
+++ epi_cpp/16_HonorsClass/47_test_arbitrage.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <limits>
 
 using namespace std;
 
-bool BellmanFord(const vector<vector<double>>& G, int source){
+bool BellmanFord(const vector<vector<double>> G, int source){
     vector<double> dis_to_source(G.size(), numeric_limits<double>::max());
     dis_to_source[source] = 0;
     for(size_t times=1; times < G.size(); ++times){
@@ -22,9 +23,8 @@ bool BellmanFord(const vector<vector<double>>& G, int source){
             return false;
         }
     }
-    // Detects cycle
     for(size_t i=0; i < G.size(); ++i){
-        for(size_t j=0; j < G[i].size(); ++j){
+        for(size_t j=0; j < G.size(); ++j){
             if(dis_to_source[i] != numeric_limits<double>::max() &&
                dis_to_source[j] > dis_to_source[i] + G[i][j]){
                 return true;
@@ -44,5 +44,19 @@ bool IsArbitrageExist(vector<vector<double>> G){
 }
 
 int main(){
+    vector<vector<double>> G{
+        {1, 0.8123, 0.6404, 78.125, 0.9784, 0.9924, 0.9465},
+        {1,2275, 1, 0.7860, 96.55, 1.2010, 1.2182, 1.1616},
+        {1.5617, 1.2724, 1, 122.83, 1.5280, 1.5498, 1.4778},
+        {0.0128, 0.0104, 0.0081, 1, 1.2442, 0.0126, 0.0120},
+        {1.0219, 0.8327, 0.6546, 80.39, 1, 1.0142, 0.9672},
+        {1.0076, 0.8206, 0.6453, 79.26, 0.9859, 1, 0.9535},
+        {1.0567, 0.8609, 0.6767, 83.12, 1.0339, 1.0487, 1}
+    };
+    if(IsArbitrageExist(G)){
+        cout << "Arbitrage exists"<<endl;
+    }else{
+        cout << "Arbitrage doesn't exist"<<endl;
+    }
     return 0;
 }
