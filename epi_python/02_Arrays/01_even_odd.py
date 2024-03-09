f30be53c4186388e8e842cc7434e7c4b935981aa
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2024-03-09 20:25:53 +0530

    even odd

---
+++ epi_python/02_Arrays/01_even_odd.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def even_odd(A: List[int]) -> None:
+    next_even, next_odd = 0, len(A) - 1
+    while next_even < next_odd:
+        if A[next_even]%2 == 0:
+            next_even += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
+
+if __name__ == '__main__':
+    A = [1,2,3,4,5,6,7]
+    even_odd(A)
+    for a in A:
+        print(a)
