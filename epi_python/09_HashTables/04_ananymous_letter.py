317f4e86abc7507b5dd1de54a720797a3d90709b
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2024-03-16 23:58:34 +0530

    anonymous letter

--- epi_python/09_HashTables/04_ananymous_letter.py
+++ epi_python/09_HashTables/04_ananymous_letter.py
@@ -1,4 +1,12 @@
 import collections
 
 def is_letter_constructible_from_magzine(letter_text: str, magzine_text: str)->bool:
-    
+    char_frequency_for_letter = collections.Counter(letter_text)
+    for c in magzine_text:
+        if c in char_frequency_for_letter:
+            char_frequency_for_letter[c] -= 1
+            if char_frequency_for_letter[c] == 0:
+                del char_frequency_for_letter[c]
+                if not char_frequency_for_letter:
+                    return True
+    return not char_frequency_for_letter
