88aacb031172f7ab3ff9785fdd65f54e36b3ad01
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2024-04-13 10:31:11 +0530

    dutch flag

--- epi_python/02_Arrays/04_dutch_flag_b.py
+++ epi_python/02_Arrays/04_dutch_flag_b.py
@@ -1,16 +1,21 @@
-from typing import List, Iterator
-import itertools
-import heapq
+from typing import List
 
-def top_k(k: int, stream: Iterator[str])->List[str]:
-    min_heap = [(len(s), s) for s in itertools.islice(stream, k)]
-    heapq.heapify(min_heap)
-    for next_string in stream:
-        heapq.heappushpop(min_heap, (len(next_string), next_string))
-    return [p[1] for p in heapq.nsmallest(k, min_heap)]
+def dutch_flag_partition(pivot_index : int, A: List[int])->None:
+    pivot = A[pivot_index]
+    smaller, equal, larger = 0, 0, len(A)
+    while equal < larger:
+        if A[equal] < pivot:
+            A[smaller], A[equal] = A[equal], A[smaller]
+            smaller, equal = smaller + 1, equal + 1
+        elif A[equal] == pivot:
+            equal += 1
+        else:
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
 
 if __name__ == '__main__':
-    strings = ["this", "is", "the", "way", "street", "approximate"];
-    string_iter = iter(strings)
-    for s in top_k(3, string_iter):
-        print(s)
+    A = [0, 1, 2, 0, 1, 2, 0, 1, 2]
+    pivot_index = 1
+    dutch_flag_partition(pivot_index, A)
+    for a in A:
+        print(a)
