71a3b86691e7d5b166267d1e4f98e977be2c8a8a
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2024-06-27 00:27:37 +0530

    update

--- epi_cpp/16_HonorsClass/20_search_in_two_sorted_arrays.cpp
+++ epi_cpp/16_HonorsClass/20_search_in_two_sorted_arrays.cpp
@@ -7,7 +7,18 @@ int FindKthInTwoSortedArrays(const vector<int>& A, const vector<int>& B, int k){
     int b = max(0, static_cast<int>(k - B.size()));
     int t = max(static_cast<int>(A.size()), k);
     while(b < t){
-
+        int x = b + ((t - b)/2);
+        int A_x_1 = (x <= 0 ? numeric_limits<int>::min() : A[x-1]);
+        int A_x = (x >= A.size() ? numeric_limits<int>::max() : A[x]);
+        int B_k_x_1 = (k - x <= 0 ? numeric_limits<int>::min() : B[k - x - 1]);
+        int B_k_x = (k - x >= B.size() ? numeric_limits<int>::max() : B[k-x]);
+        if(A_x < B_k_x_1){
+            b = x + 1;
+        }else if(A_x_1 > B_k_x){
+            t = x - 1;
+        }else{
+            return max(A_x_1, B_k_x_1);
+        }
     }
     int A_b_1 = (b <= 0)? numeric_limits<int>::min() : A[b-1];
     int B_k_b_1 = (k - b - 1 < 0) ? numeric_limits<int>::min() : B[k-b-1];
