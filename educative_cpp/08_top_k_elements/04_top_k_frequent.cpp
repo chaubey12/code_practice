ff9dbbdeeda5a3220aa618631de57bb7f0650e95
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2024-09-28 10:25:04 +0530

    top k

--- educative_cpp/08_top_k_elements/04_top_k_frequent.cpp
+++ educative_cpp/08_top_k_elements/04_top_k_frequent.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
+#include <queue>
 
 using namespace std;
 
@@ -10,3 +11,34 @@ struct Comp{
     }
 };
 
+vector<int> TopKFrequent(const vector<int>& nums, int k){
+    unordered_map<int, int> numFrequencyMap;
+    for(int num : nums){
+        ++numFrequencyMap[num];
+    }
+    priority_queue<pair<int, int>, vector<pair<int, int>>, Comp> topKElements;
+    for(auto& entry : numFrequencyMap){
+        topKElements.emplace(entry);
+        if(topKElements.size() > k){
+            topKElements.pop();
+        }
+    }
+    vector<int> result;
+    while(!topKElements.empty()){
+        result.emplace_back(topKElements.top().first);
+        topKElements.pop();
+    }
+    return result;
+}
+
+
+int main(){
+    vector<int> nums{1, 3, 5, 12, 11, 12, 11, 12, 5, 1, 3, 5, 14, 18, 14, 5};
+    int k = 3;
+    auto result = TopKFrequent(nums, k);
+    for(int a : result){
+        cout << a << " ";
+    }
+    cout << endl;
+    return 0;
+}
