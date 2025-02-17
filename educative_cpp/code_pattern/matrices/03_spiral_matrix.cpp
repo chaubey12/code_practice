eb584a317a3496309606a26769d1491ecdf64faf
chaubey12 <chaubey_mm@yahoo.co.in>
2025-02-17 10:37:28 +0530

    probs

---
+++ educative_cpp/code_pattern/matrices/03_spiral_matrix.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> SpiralOrder(vector<vector<int>>& matrix){
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    int row = 0;
+    int col = -1;
+    int direction = 1;
+    vector<int> result;
+    while(rows > 0 && cols > 0){
+        for(int i = 0; i < cols; ++i){
+            col += direction;
+            result.emplace_back(matrix[row][col]);
+        }
+        rows--;
+        for(int i = 0; i < rows; ++i){
+            row += direction;
+            result.emplace_back(matrix[row][col]);
+        }
+        cols--;
+        direction *= -1;
+    }
+    return result;
+}
+
+int main(){
+    return 0;
+}
