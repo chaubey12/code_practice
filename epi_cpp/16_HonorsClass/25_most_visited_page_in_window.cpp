2a8644db1cadbe0b525e33a61097ca6489c484ec
Madhup Choubey <madhupchoubey@Madhups-MacBook-Air.local>
2025-02-22 15:02:32 +0530

    long pending

--- epi_cpp/16_HonorsClass/25_most_visited_page_in_window.cpp
+++ epi_cpp/16_HonorsClass/25_most_visited_page_in_window.cpp
@@ -1,1 +1,40 @@
 // PLACEHOLDER
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<pair<string, int>> KMostVisitedPagesInWindow(const vector<string>& pageVisits, int k, int w){
+    unordered_map<string, int> visitCount;
+    deque<string> window;
+    for(const string& page : pageVisits){
+        window.emplace_back(page);
+        ++visitCount[page];
+        if(window.size() > w){
+            string oldestPage = window.front();
+            window.pop_front();
+            if(--visitCount[oldestPage] == 0){
+                visitCount.erase(oldestPage);
+            }
+        }
+    }
+    priority_queue<pair<int, string>, vector<pair<int, string>>, greater<int>> minHeap;
+    for(const auto& [page, count] : visitCount){
+        minHeap.push({count, page});
+        if(minHeap.size() > k){
+            minHeap.pop();
+        }
+    }
+    
+    vector<pair<string, int>> result;
+    while(!minHeap.empty()){
+        result.push_back({minHeap.top().second, minHeap.top().first});
+        minHeap.pop();
+    }
+    reverse(result.begin(), result.end());
+    return result; 
+}
